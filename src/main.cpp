ceb859a84d19f00a5e6aff961e2d741daf3b7ccc
Kyle Givler <xvegan88x@gmail.com>
2014-07-26 23:22:49 -0400

    test parsing with boost property tree and libtidy

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,64 @@
 #include <boost/thread/thread.hpp>
 #include "http_client.hpp"
 #include "http_request.hpp"
+#include <tidy.h>
+#include <buffio.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+ 
+ 
+struct html_test
+{
+  std::string title;
+  void load(std::istringstream &stream)
+  {
+    using boost::property_tree::ptree;
+    ptree pt;
+    read_xml(stream, pt);
+    
+    title = pt.get("html.head.title", "null");
+    return;
+  }
+};
+ 
+std::string CleanHTML(const std::string &html){
+  // Initialize a Tidy document
+  TidyDoc tidyDoc = tidyCreate();
+  TidyBuffer tidyOutputBuffer = {0};
+
+  // Configure Tidy
+  // The flags tell Tidy to output XML and disable showing warnings
+  bool configSuccess = tidyOptSetBool(tidyDoc, TidyXmlOut, yes)
+      && tidyOptSetBool(tidyDoc, TidyQuiet, yes)
+      && tidyOptSetBool(tidyDoc, TidyNumEntities, yes)
+      && tidyOptSetBool(tidyDoc, TidyShowWarnings, no);
+
+  int tidyResponseCode = -1;
+
+  // Parse input
+  if (configSuccess)
+      tidyResponseCode = tidyParseString(tidyDoc, html.c_str());
+
+  // Process HTML
+  if (tidyResponseCode >= 0)
+      tidyResponseCode = tidyCleanAndRepair(tidyDoc);
+
+  // Output the HTML to our buffer
+  if (tidyResponseCode >= 0)
+      tidyResponseCode = tidySaveBuffer(tidyDoc, &tidyOutputBuffer);
+
+  // Any errors from Tidy?
+  if (tidyResponseCode < 0)
+      throw ("Tidy encountered an error while parsing an HTML response. Tidy response code: " + tidyResponseCode);
+
+  // Grab the result from the buffer and then free Tidy's memory
+  std::string tidyResult = (char*)tidyOutputBuffer.bp;
+  tidyBufFree(&tidyOutputBuffer);
+  tidyRelease(tidyDoc);
+
+  return tidyResult;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -11,7 +69,7 @@ int main(int argc, char **argv)
   boost::thread_group threads; 
    
   http_client hc(io_service);
-  http_request rq("www.google.com", "/sadasd");
+  http_request rq("www.google.com", "/");
   http_request rq2("reddit.com");
   rq.set_request_type(RequestType::GET);
   hc.make_request(rq);
@@ -35,8 +93,14 @@ int main(int argc, char **argv)
     std::cout << header;
   std::cout << "\n";
   
-  std::cout << rq.get_data() << std::endl;
+  //std::cout << rq.get_data() << std::endl;
+  std::cout << CleanHTML(rq.get_data().c_str());
   
+  std::istringstream ss;
+  ss.str(CleanHTML(rq.get_data().c_str()));
+  html_test test;
+  test.load(ss);
+  std::cout << "TITLE: " << test.title << std::endl;
   
   return 0;
 }
